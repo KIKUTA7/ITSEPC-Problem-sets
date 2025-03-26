1bbc20063ee0b94f74e23a8fabab9f71960a4152
Beqa Kikutadze <96978103+KIKUTA7@users.noreply.github.com>
2025-03-26 23:35:06 +0400

    Assignment 1

--- PS0/src/turtlesoup.ts
+++ PS0/src/turtlesoup.ts
@@ -13,11 +13,11 @@ export function drawSquare(turtle: Turtle, sideLength: number): void {
   turtle.forward(sideLength);
   turtle.turn(90);
   turtle.forward(sideLength);
-  turtle.turn(180);
+  turtle.turn(90);
   turtle.forward(sideLength);
   turtle.turn(90);
   turtle.forward(sideLength);
-  turtle.turn(400);
+  turtle.turn(90);
 }
 
 /**
@@ -29,7 +29,7 @@ export function drawSquare(turtle: Turtle, sideLength: number): void {
  */
 export function chordLength(radius: number, angleInDegrees: number): number {
   // TODO: Implement chordLength
-  return 0; // Placeholder - replace with your implementation
+  return parseFloat((Math.sqrt(4 * Math.pow(radius, 2) * Math.pow(Math.sin(angleInDegrees * Math.PI / 360), 2))).toFixed(10));
 }
 
 /**
@@ -45,6 +45,12 @@ export function drawApproximateCircle(
   numSides: number
 ): void {
   // TODO: Implement drawApproximateCircle
+  
+  for (let i:number = 0; i < numSides; i++)
+    {
+      turtle.forward(chordLength(radius, 360 / numSides));
+      turtle.turn(180 - 180 * (numSides - 2) / numSides);
+    }
 }
 
 /**
@@ -55,7 +61,7 @@ export function drawApproximateCircle(
  */
 export function distance(p1: Point, p2: Point): number {
   // TODO: Implement distance
-  return 0; // Placeholder
+  return Math.sqrt(Math.pow(p1.x - p2.x,2) + Math.pow(p1.y - p2.y,2));
 }
 
 /**
@@ -69,7 +75,31 @@ export function distance(p1: Point, p2: Point): number {
  */
 export function findPath(turtle: Turtle, points: Point[]): string[] {
   // TODO: Implement findPath (conceptually, you don't need to *execute* the path here)
-  return []; // Placeholder
+  let result: string[] = [];
+  for (let i = 0; i < points.length - 1; i++) {
+    let curr_point = points[i];
+    let next_point = points[i + 1];
+
+    console.log(curr_point, next_point);
+
+    let dist = distance(curr_point, next_point);
+
+    let opposite = (curr_point.y - next_point.y);
+    let adjacent = (curr_point.x - next_point.x);
+
+    let angle_radians = Math.atan2(opposite, adjacent);
+    let angle_degrees = (angle_radians * 180) / Math.PI + 90;
+
+    let curr_heading = turtle.getHeading()
+
+    turtle.turn(0 - curr_heading + angle_degrees - 90);
+    turtle.forward(dist);
+    result.push("forward " + dist);
+    angle_degrees = angle_degrees > 180 ? angle_degrees - 180 : angle_degrees;
+    result.push("turn " + angle_degrees);
+    }
+
+    return result.reverse();
 }
 
 /**
@@ -82,9 +112,14 @@ export function findPath(turtle: Turtle, points: Point[]): string[] {
 export function drawPersonalArt(turtle: Turtle): void {
   // TODO: Implement drawPersonalArt
   // Example - replace with your own art!
-  for (let i = 0; i < 6; i++) {
-    turtle.forward(50);
-    turtle.turn(60);
+  const colors: Color[] = ["red", "orange", "yellow", "green", "blue", "purple"];
+  for (let step = 0; step < 50; step++) {
+    turtle.color(colors[step % colors.length]);
+    for (let i = 0; i < 6; i++) {
+      turtle.forward(50 + step);
+      turtle.turn(60);
+    }
+    turtle.turn(10);
   }
 }
 
